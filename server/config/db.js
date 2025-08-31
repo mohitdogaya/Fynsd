3c9794bc142594d04a81274a3fada6abd5a5f26e
Mohit Dogaya <mohitdogaya6@gamial.com>
2025-08-31 19:59:47 +0530

    deploy

--- server/config/db.js
+++ server/config/db.js
@@ -6,11 +6,14 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("MongoDB Connected");
-  } catch (error) {
-    console.error("MongoDB Connection Failed:", error.message);
-    process.exit(1); // Stop process if DB not connected
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ MongoDB connected successfully");
+  } catch (err) {
+    console.error("❌ MongoDB connection failed", err.message);
+    process.exit(1);
   }
 };
 
